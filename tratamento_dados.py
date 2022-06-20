96288432d885b0741575ce3b414a3f8aeaf9b3eb
Ackio-Crypt <joaopedro.jf90@gmail.com>
2022-06-19 22:13:38 -0300

    Update tratamento_dados.py

--- tratamento_dados.py
+++ tratamento_dados.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib as plt
 
 base = pd.read_csv('/home/ackio/Documentos/Projects ACK/Python/Gerador_Graf/Graphic_Generator/indice.csv')
-
-print(base['NumAno'])
+df = pd.DataFrame(base)
+print(df['NumAno'])
 
 
 
