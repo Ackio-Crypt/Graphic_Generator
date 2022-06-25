ef9463fd0ecb5400181faae28545a7beb326b9d2
Ackio-Crypt <joaopedro.jf90@gmail.com>
2022-06-25 19:59:31 -0300

    1.1
    Código funcional para plotar gráficos com parâmetros simples

--- tratamento_dados.py
+++ tratamento_dados.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-base = pd.read_csv('/home/ackio/Documentos/Projects ACK/Python/Gerador_Graf/Graphic_Generator/indice.csv')
+#Utilizando a biblioteca Pandas, é possível realizar um tratamento simples na base
+#de dados utilizando os parâmetros adequados
+base = pd.read_csv('Arquivo', skiprows=33, delimiter='\t')
+
+#Após a leitura numa variável, utiliza-se a função DataFrame para realizar o tratamento
 df = pd.DataFrame(base)
-df['NumAno'].plot()
 
+df_trat = df.loc[1050:] #Notação para iniciar a partir de determinado valor de linha
 
-
+#Plot Gráfico
+df_trat.plot()
+plt.show()
